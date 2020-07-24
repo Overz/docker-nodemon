fc5bbbfe82f2f2e6aa8b3035bb55fb6b5448dc6b
Christian Lima <christian@projetabrasil.net.br>
2020-07-23 23:04:58 -0300

    using html file into js app

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
-const port = process.env.port || '3000';
-const host = process.env.HOST || '0.0.0.0';
+const port = process.env.port || "3000";
+const host = process.env.HOST || "0.0.0.0";
+const p = __dirname.replace("js", "html") + "/index.html";
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hellow World');
+app.get("/", (req, res) => {
+  res.sendFile(p);
 });
 
 app.listen(port, host);
